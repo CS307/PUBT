41f5dd854cb726ec1bf6e8ff7a53100bfa6f1209
Tianyang Yu <yutianyang94@gmail.com>
2014-03-06 14:47:33 -0500

    update public/css/resultpage.css

--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -8,10 +8,18 @@
 			@endif
 			<div class="row">
 		@endif
-		<div class="col-xs-6 col-md-3">
-			<a href="#" class="thumbnail">
-				<img data-src="holder.js/100%x180" alt="sampleimg.jpg" src="{{asset('css/sampleimg.jpg')}}">
-			</a>
+		<div class="col-xs-6 col-md-3" class=picboxding>
+			<div class=picboxding>
+				<a href="#" class="thumbnail">
+					<img data-src="holder.js/100%x180" alt="sampleimg.jpg" src="{{asset('css/sampleimg.jpg')}}">
+				</a>
+				<div class="caption" class=imginfoding>
+					<h3 class=imginfoding>tasiod</h3>
+					<p class=imginfoding>info1</p>
+					<p class=imginfoding>info2</p>
+					<p>info3</p>
+				</div>
+		    </div>
 		</div>
 	@endfor
     </div>
