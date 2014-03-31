ac59d65803934e12ba70e176b108dd5a8c279c39
li1173 <li1173@purdue.edu>
2014-03-31 13:06:01 -0400

    profile page update

--- app/views/profilePage.blade.php
+++ app/views/profilePage.blade.php
@@ -6,35 +6,77 @@
 
 @section('base_content')
 <div class=container>
+	<div class="wrapper">
 		<div class=headding>
 			<div class="col-md-1">
+				<div class="media">
+					<a class="pull-right" href="#">
+						<img height="31" width="66" class="media-object" src="{{asset('css/logo.jpg')}}" alt="...">
+					</a>
+				</div>
 			</div>
 			<div class="col-md-11">
 				<p class=pheadding>Welcome, DingYEYE</p>
 				<p class=ph2ding>Your are currently logged in.</p>
 			</div>
 		</div>
+	<div class="content-book-ding">
 	<div class="row">
-		<div class="col-md-2">
+		<div class="col-md-3">
 			<aside class=leftding>
-				<h1 class=h1ding>Quick Links</h1>
-				<p class=pding>Informations: </p>
-				<a href="#">Personal Info</a><br>
-				<a href="#">Address & phones</a><br>
-				<a href="#">Email Reminds</a><br>
-				<a href="#">Credit Record</a><br>
-				<p class=pding>Account: </p>
-				<a href="#">Change Password</a><br>
-				<a href="#">Follow List</a><br>
-				<a href="#">Buyer List</a><br>
+				<h1 class=h1ding>Personal Infomations: </h1>
+				<p class=pding>Name:</p>
+				<p class=pding>Major:</p>
+				<p class=pding>Phone Number:</p>
 			</aside>
 		</div>
-		<div class="col-md-5">
-	
+		<div class="col-md-4">
+			<div class=topding>
+			<div class="panel panel-warning">
+				<div class="panel-heading">
+					<h3 class="panel-title">Followed Books:</h3>
+				</div>
+				<div class=panelbodyding>
+					<div class="row">
+						<div class="col-sm-6 col-md-5">
+							<a href="#" class="thumbnail">
+								<img data-src="holder.js/240x160" src="{{asset('css/sampleimg.jpg')}}" alt="sampleimg">
+							</a>
+						</div>
+						<div class="col-md-7">
+							<div class=tbreakding>
+								<p>Info:</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
 		</div>
-		<div class="col-md-5">
-	
+		
+		<div class="col-md-4">
+			<div class=topding>
+			<div class="panel panel-success">
+				<div class="panel-heading">
+					<h3 class="panel-title">Selling Books:</h3>
+				</div>
+				<div class=panelbodyding>
+					Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>
+					Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>
+					Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>Panel content<br>
+					
+				</div>
+			</div>
+			</div>
 		</div>
+	</div>
+	</div>
+	</div>
+	
+	<div class="row">
+		<footer class=footerding>
+			Copyright: PUBT: Dingzhe Li-Zorak!
+		</footer>
 	</div>
 </div>
 @stop
