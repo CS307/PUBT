b7196236e30927011d577fd5fed77b386cbcd30b
Tianyang Yu <yutianyang94@gmail.com>
2014-04-03 22:26:57 -0400

    Change post_page.blade.php

--- app/views/post_page.blade.php
+++ app/views/post_page.blade.php
@@ -1,143 +1,239 @@
-@extends('base')
 
-@section('base_link')
-<link href="{{asset('css/postpage.css')}}" rel="stylesheet">
-@stop
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>The Most Poplular Local Trading Site!</title>
 
-@section('base_content')
+<link href="bootstrap.min.css" rel="stylesheet">
+<link href="sellingpage.css" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-latest.min.js"
+        type="text/javascript"></script>
+<script src="bootstrap.min.js"></script>
+<style>
+body{padding: 50px;}
+</style>
 
-<div class="container mainsection">
-  <h1 class=h1ding>Post your book:</h1>
-	<div class="row-fluid">
-		<!--form id="user-new-address" action="/logistics/address/" method="post">
-	
-		<div class="partinfo">
-			<h3 class="heading-3">Book info:</h3>
-				
-		<div class="single">
-			<label class="control-label" for="recipient_name">Title:</label>
-			<div class="controls">
-			<input type="text" class="input-xlarge" id="recipient_name" name="recipient_name">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-		<div class="single">
-			<label class="control-label" for="recipient_name">Author:</label>
-			<div class="controls">
-			<input type="text" class="input-xlarge" id="author" name="author">
-			<p class="help-inline"></p>
-		</div>
-		</div>
-		
-		<div class="single">
-			<label class="control-label" for="phone">Co-course Number:</label>
-			<div class="controls">
-			<input type="text" class="input-xlarge" id="CN" name="CN">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-	
-		<div class="single">
-			<label class="control-label" for="phone">Listing Price:</label>
-			<div class="controls">
-			<input type="text" class="input-xlarge" id="price" name="price">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-		
-		<div class="single">
-			<label class="control-label" for="phone">Condition:</label><br>
-			<div class="btn-group btn-group-lg">
-  				<div class="btn-group">
-    			<button type="button" class="btn btn-default">New with tags</button>
-  				</div>
-  				<div class="btn-group">
-    			<button type="button" class="btn btn-default">Like new</button>
-  				</div>
-  				<div class="btn-group">
-    			<button type="button" class="btn btn-default">Good condition</button>
-  				</div>
-  				<div class="btn-group">
-    			<button type="button" class="btn btn-default">Fair condition</button>
-  				</div>
-			</div>
-		</div>
-		
-		<div class="single">
-			<label class="control-label" for="phone">Discription:</label>
-			<div class="controls">
-			<input type="text" class="input-xlarge" id="price" name="price">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-		</div>	
-		
-		<div class="single">
-		<a href="#" class="btn btn-success" type="post">
-			<i class="glyphicon glyphicon-plus"></i>
-			Submit&Post</a>
-		</div>
-		<form-->
-		{{Form::open(array('url'=>'postPost','method'=>'post'))}}
-              <div class="partinfo">
-			  <h3 class="heading-3">Book info:</h3>
-				
-		<div class="single">
-			<label class="control-label">Title:</label>
-			<div class="controls">
-<input type="text" name="title">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-		<div class="single">
-			<label class="control-label">Author:</label>
-			<div class="controls">
-<input type="text" name="author">
-			<p class="help-inline"></p>
-		</div>
-		</div>
-		
-		<div class="single">
-			<label class="control-label">Co-course Number:</label>
-			<div class="controls">
-<input type="text" name="CN">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-	
-		<div class="single">
-			<label class="control-label">Listing Price:</label>
-			<div class="controls">
-<input type="text" name="price">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-		
-		<div class="single">
-			<label class="control-label">Listing Condition:</label>
-			<div class="controls">
-<input type="text" name="condition">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-		
-		<div class="single">
-			<label class="control-label">Description:</label>
-			<div class="controls">
-<input type="text" name="description">
-			<p class="help-inline"></p>
-			</div>
-		</div>
-		</div>	
-		
-		<div class="single">
-		<button type="submit" class="btn btn-primary" role="post">
-        <i class="glyphicon glyphicon-search"></i>
-        Post</button>
-		</div>
-        {{Form::close()}}
+</head>
+<head>
 
-		</div>
-		</div>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+  <div class=container>
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">PUBT</a>
+    </div>
 
-@stop
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav ">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">MA</a></li>
+        <li><a href="#">CS</a></li>
+        <li><a href="#">ENG</a></li>
+        <li><a href="#">PHY</a></li>
+        <li><a href="#">MGMT</a></li>
+        <li><a href="#">STAT</a></li>
+        <li><a href="#">Others</a></li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search" action="#">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="In format:CS 180">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      
+      
+      <ul class="nav navbar-nav navbar-right pull-right"> 
+    
+    <li class="dropdown" id="menuLogin">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navSignup">Join us<b class="caret"></b></a>
+            <div class="dropdown-menu" style="padding:17px;">
+              <form class="form" id="formSignup" action="#" method="post"> 
+                <input name="username" id="username" type="text" placeholder="Username">
+                <input name="password" id="password" type="password" placeholder="Password"><br>
+                <input name="password" id="passwordconfirm" type="passwordconfirm" placeholder="Confirm Password"><br>
+                <button type="submit" id="btnLogin" class="btn">Sign up</button>
+              </form>
+            </div>
+          </li>
+    
+        
+        <li class="dropdown" id="menuLogin">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Sign in<b class="caret"></b></a>
+            <div class="dropdown-menu" style="padding:17px;">
+              <form class="form" id="formLogin" action="#" method="post"> 
+                <input name="username" id="username" type="text" placeholder="Username">
+                <input name="password" id="password" type="password" placeholder="Password"><br>
+                <button type="submit" id="btnLogin" class="btn">Login</button>
+              </form>
+            </div>
+          </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+ </div><!-- /.container -->
+</nav>
+
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+  <div class=container>
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">PUBT</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav ">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">MA</a></li>
+        <li><a href="#">CS</a></li>
+        <li><a href="#">ENG</a></li>
+        <li><a href="#">PHY</a></li>
+        <li><a href="#">MGMT</a></li>
+        <li><a href="#">STAT</a></li>
+        <li><a href="#">Others</a></li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search" action="#">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="In format:CS 180">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      
+      
+      <ul class="nav navbar-nav navbar-right pull-right"> 
+    
+    <li class="dropdown" id="menuLogin">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navSignup">Join us<b class="caret"></b></a>
+            <div class="dropdown-menu" style="padding:17px;">
+              <form class="form" id="formSignup" action="#" method="post"> 
+                <input name="username" id="username" type="text" placeholder="Username">
+                <input name="password" id="password" type="password" placeholder="Password"><br>
+                <input name="password" id="passwordconfirm" type="passwordconfirm" placeholder="Confirm Password"><br>
+                <button type="submit" id="btnLogin" class="btn">Sign up</button>
+              </form>
+            </div>
+          </li>
+    
+        
+        <li class="dropdown" id="menuLogin">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Sign in<b class="caret"></b></a>
+            <div class="dropdown-menu" style="padding:17px;">
+              <form class="form" id="formLogin" action="#" method="post"> 
+                <input name="username" id="username" type="text" placeholder="Username">
+                <input name="password" id="password" type="password" placeholder="Password"><br>
+                <button type="submit" id="btnLogin" class="btn">Login</button>
+              </form>
+            </div>
+          </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+ </div><!-- /.container -->
+</nav>
+
+
+<div class="container">
+  <div class="row">
+    <div class = "col-md-5 leftpart"> 
+      <h2>Title:Think Into Java?</h2>
+      <h4>corresponding course:
+        <span class="label label-info">CS 18000</span>
+      </h4>
+      <div class = "row">
+        <div class = "col-md-7">
+           <a href="#" class="thumbnail">
+           <img data-src="holder.js/100%x180" alt="sampleimg.jpg" src="sampleimg.jpg">
+           </a>
+        
+        <h3 class="heading bold">Item details:</h3>
+        </div>
+      <table id="table_item">
+        <tr>
+          <td class="name">Author: </td>
+          <td class="value">Buster Dunsmore</td>
+        </tr>
+        <tr>
+          <td class="name">ISBN: </td>
+          <td class="value">52GSJDF1234JSA</td>
+        </tr>
+      </table>
+      </div>
+  </div>
+
+
+  <div class = "col-md-6 rightpart" style ="margin-top: 20px;"> 
+    <h6 class = "sofia">You can either contact the seller to buy or add the book to your follow list</h6>
+    <hr>
+
+    <div class = "buyfollow">
+     <div class = "row">
+      <div class = "col-md-12">
+        <h3 class = "subtitle">Post Book</h3>
+        <div class = "contactleft" style="margin-top: 0px;">
+          <div class="controlgroup">
+
+            <table id="table_item">
+              <tr>
+              <td class="name">List Price: </td>
+              <td class="value">
+                  <div class ="priceinput">
+                   <span class="add-on sofia">$</span>
+                    <input type="text" name="amount" placeholder="-" style = "line-height: 25px">
+                  </div>
+              </td>
+              </tr>
+        
+              <tr>
+              <td class="name">Condition: </td>
+              <td class="value">
+                <select class="form-control">
+                 <option>New With Tags</option>
+                 <option>Like New</option>
+                 <option>Good Condition</option>
+                 <option>Fair Condition</option>
+                </select>
+              </td>
+              </tr>
+
+              <tr>
+              <td class="name">Description: </td>
+              <td class="value">
+                  <div class ="priceinput myinput">
+            <textarea type="text" name="amount" placeholder="Why you sell it? Detail of the condition etc.."></textarea>
+            </div>
+              </td>
+              </tr>
+      </table>
+            
+          </div>
+        </div>
+        <a href="#" class="btn btn-contact" style="margin-top: 10px;"><i class="icon-white icon-plus"></i>Post Book
+        </a>
+      </div>
+     </div>
+    </div>
+<!--
+    <div class = "buyfollow">
+     <div class = "row">
+      <h3 class = "subtitle" style = "padding-left: 14px;">Similar Items</h3>
+     </div>
+    </div>
+  </div>
+-->
+
+</div>
+</div>
+</div>
+</body>
+</html>
