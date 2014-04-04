e4c71c4dad54abc83e867637eb1461fab7f86980
Tianyang Yu <yutianyang94@gmail.com>
2014-04-04 01:18:10 -0400

    minor change in search behavior

--- app/views/book_copys_results.blade.php
+++ app/views/book_copys_results.blade.php
@@ -10,12 +10,11 @@
 		@endif
 		<div class="col-xs-6 col-md-3" class=picboxding>
 			<div class=picboxding>
-				<a href="#" class="thumbnail">
+				<a href="/search/book_copy_id={{$results[$count]->id}}" class="thumbnail">
 					<img data-src="holder.js/100%x180" alt="sampleimg.jpg" src="{{asset('css/sampleimg.jpg')}}">
 				</a>
 				<div class="caption" class=imginfoding>
-					<h5 class=imginfoding>{{ $results[$count]->title }}</h5>
-					<p class=imginfoding>Price: ${{ $results[$count]->price}}</p>
+					<p class=imginfoding>Price: ${{ $results[$count]->price }}</p>
 					<p class=imginfoding>Seller: {{ DB::table('users')->where('id',$results[$count]->seller_id)->first()->email }}</p>
 					<p class=imginfoding>Condition: {{ $results[$count]->condition }}</p>
 				</div>
