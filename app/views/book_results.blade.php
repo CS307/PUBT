5738ee4989b2bd43193e51ecf4337c3719bec4b1
li1173 <li1173@purdue.edu>
2014-04-23 19:01:06 -0400

    result page update

--- app/views/book_results.blade.php
+++ app/views/book_results.blade.php
@@ -1,28 +1,36 @@
 @extends('base')
 
+@section('base_link')
+<link href="{{asset('css/resultpage.css')}}" rel="stylesheet">
+@stop
+
 @section('base_content')
-	<h1>Choose the book you want to search for: </h1>
+	<h1 class="topicding">Choose the book you want to search for: </h1>
 	@for($count=0;$count < count($results);$count++)
-		@if($count%4==0)
+		@if($count%5==0)
 			@if($count!=0)
+				<div class="col-md-1">
+				</div>
 				</div>
 			@endif
 			<div class="row">
+			<div class="col-md-1">
+			</div>
 		@endif
-		<div class="col-xs-6 col-md-3" class=picboxding>
+		<div class="col-md-2" class=picboxding>
 			<div class=picboxding>
 				<div class="imgcontainer">
 					<a href="/{{ $method }}/book_id={{ $results[$count]->id }}" class="thumbnail imgcontainer">
 						@if($results[$count]->pic_url == "NO_URL" || $results[$count]->pic_url == "")
-							<img data-src="holder.js" max-height="200" alt="Sorry, No Cover" src="http://www.slugbooks.com/images/notavail2.png">
+							<img data-src="holder.js" max-width="200" max-height="200" alt="Sorry, No Cover" src="http://www.slugbooks.com/images/notavail2.png">
 						@else
-							<img data-src="holder.js" max-height="200" alt="Sorry, No Cover" src="{{$results[$count]->pic_url}}">
+							<img data-src="holder.js" max-width="200" max-height="200" alt="Sorry, No Cover" src="{{$results[$count]->pic_url}}">
 						@endif
 					</a>
 				</div>
 				<div class="caption imginfo">
-					<h5 class=imginfoding style="height: 32px; vertical-align:text-top;"><b>{{ $results[$count]->title }}</b></h5>
-					<p class=imginfoding style="height: 22px; vertical-align:text-top;"><b>Author:</b> {{ $results[$count]->author }}</p>
+					<h5 class=imginfoding><b>{{ $results[$count]->title }}</b></h5>
+					<p class=imginfoding><b>Author:</b> {{ $results[$count]->author }}</p>
 					<p class=imginfoding><b>Course:</b> {{ $results[$count]->subject.' '.$results[$count]->course_id }}</p>
 					<p class=imginfoding><b>Edition:</b> {{ $results[$count]->edition }}</p>
 				</div>
